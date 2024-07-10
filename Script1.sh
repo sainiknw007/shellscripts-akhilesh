8f4bbb770aabca20f1c8bae1dbd595f41e555b42
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2024-07-10 11:07:34 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,5 +2,5 @@
 #This is my 1st shell script to print output
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
-echo "Welcome to Young Minds"
-echo "Hello batch-21, We are learning Jenkins"
+echo "Welcome to AWS-DEVOPS"
+echo "Hello batch-1, We are learning AWS DevOps"
